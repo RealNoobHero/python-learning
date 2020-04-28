d333499c51b044836cbc0e40adbd0c6fe75c8a2f
Mark <64454420+RealNoobHero@users.noreply.github.com>
2020-04-28 13:36:37 +0900

    Add files via upload

---
+++ dice roller1.1.py
@@ -0,0 +1,26 @@
+#dice roller
+import sys,math
+from random import randint #.1 clean up code
+while True:
+    print('Choose your dice: coin, d4, d6 d8, d10, or d20 Press Q to quit')
+    dice=input()
+    if dice==('coin'):
+        for i in range(1):
+            randomNumber=(randint(1,2)) #no random. required!
+        if randomNumber==1:
+            print(' You got Heads')
+        elif randomNumber==2:
+            print('You got Tails')
+    if dice==('d4'):
+        print('You got a '+str(randint(1,4)))#add you got a (str)
+    if dice==('d6'):
+        print('You got a '+str(randint(1,6)))
+    if dice==('d8'):
+        print('You got a '+str(randint(1,8)))
+    if dice==('d10'):
+        print('You got a '+str(randint(1,10)))
+    if dice==('d20'):
+        print('You got a '+str(randint(1,20)))
+    if dice==('q'):
+        print('Thanks for playing!')
+        sys.exit()
